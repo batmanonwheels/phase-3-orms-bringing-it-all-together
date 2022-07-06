cb55c36fbfa268aed8b276684e9ba639784b30ae
Devin Garcia <devingarcia5423@gmail.com>
2022-07-06 11:39:11 -0400

    commit tree 7c021d77bcf8b44f800d3ae10de6839e142b8201

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,71 @@
 class Dog
+  attr_accessor :id, :name, :breed
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS dogs
+      CREATE TABLE dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT,
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs" )[0][0]
+
+    self
+  end
+
+  def self.create
+    dog = Dog.new(name: name, bree: breed)
+    dog.save
+  end
+
+  def self.new_from_db arr
+    self.new(id: arr[0], name: arr[1], breed: arr[2])
+  end
+
+  def self.all
+
+  end
+
+  def self.find_by_name(name)
+
+  end
+
+  def self.find(id)
+
+  end
+
+  def self.find_or_create_by
+
+  end
+
+  def update
+
+  end
 
 end
